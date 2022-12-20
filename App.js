b8177b1d9f8bbbecb2f264e5a596b5f12ee2c2ce
Usman Farooq <m.usmanfarooq753@gmail.com>
2022-12-20 13:47:17 +0500

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [goal, setGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function enterGoalHandler(enteredText) {
+    setGoal(enteredText);
+  }
+
+  function addGoalHandler(enteredText) {
+    setCourseGoals((currentgoals) => [...currentgoals, goal]);
+    setGoal("")
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={[styles.displayflex, styles.marginbottom]}>
+        <TextInput
+          placeholder="Enter your Goal"
+          style={[styles.input, styles.shadow]}
+          onChangeText={enterGoalHandler}
+          value={goal}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
+      </View>
+      <View>
+        <Text style={styles.heading}>List of All Goals</Text>
+        {courseGoals.map((goal) => (
+          <Text style={[styles.goalstext, styles.shadow]} key={goal}>{goal}</Text>
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
+  heading: {
+    fontSize: 24,
+    fontWeight: "600",
+    marginBottom: 18,
+  },
+  displayflex: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    padding: 15,
+    backgroundColor: "whitesmoke",
+    borderRadius: 8,
+    marginRight: 10,
+    width: "70%",
+  },
+  shadow: {
+    shadowOffset: { width: -2, height: 4 },
+    shadowColor: "#171717",
+    shadowOpacity: 0.1,
+    elevation: 2,
+  },
+  marginbottom: {
+    marginBottom: 18,
+  },
+  goalstext: {
+    fontSize: 16,
+    padding: 12,
+    backgroundColor: "whitesmoke",
+    borderRadius: 8,
+    marginVertical: 5,
+  }
 });
